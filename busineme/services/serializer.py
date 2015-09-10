379719e445c18eaa314513952b1885dfdaf9a308
Gustavo Sabino <gustavo.sabino@hotmail.com>
2015-09-09 22:42:07 -0300

    change the position of the docstring

--- busineme/services/serializer.py
+++ busineme/services/serializer.py
@@ -3,15 +3,16 @@
 class Serializer():
 
     def serialize(self, busineme_object):
-        required_fields = busineme_object.serialize_fields
-        json_fields = {}
-
-        for fields in required_fields:
-            """ This method generate json based in the fields who we want to
+        """ This method generate json based in the fields who we want to
                 show or we user in the aplicattion.
 
                 busineme_object is a generic object
                 which we want to see their values """
+
+        required_fields = busineme_object.serialize_fields
+        json_fields = {}
+
+        for fields in required_fields:
 
             json_fields[fields] = getattr(required_fields, fields)
 
