4e60e4fec9bfca85d30ab32848b0a2f265d0375f
Gustavo Sabino <gustavo.sabino@hotmail.com>
2015-09-09 22:38:58 -0300

    docstring for serialize method

---
+++ busineme/services/serializer.py
@@ -0,0 +1,18 @@
+
+
+class Serializer():
+
+    def serialize(self, busineme_object):
+        required_fields = busineme_object.serialize_fields
+        json_fields = {}
+
+        for fields in required_fields:
+            """ This method generate json based in the fields who we want to
+                show or we user in the aplicattion.
+
+                busineme_object is a generic object
+                which we want to see their values """
+
+            json_fields[fields] = getattr(required_fields, fields)
+
+        return json_fields
