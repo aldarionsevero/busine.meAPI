02d24f524d70d429787569f4bda7639a45567d5e
Gustavo Sabino <gustavo.sabino@hotmail.com>
2015-09-16 11:52:15 -0300

    Create assertives in BusinemeUserAuth

--- busineme/authentication/businemeUser_authentication.py
+++ busineme/authentication/businemeUser_authentication.py
@@ -1,49 +1,52 @@
 from authentication.models import BusinemeUser
+from django.test import SimpleTestCase
 
 
-def create_user(request):
-    """PUT method for create an user by receiving
-    four arguments from application, using POST method for security"""
+class BusinemeUserAuth(SimpleTestCase):
 
-    new_user = BusinemeUser()
-    new_user.username = request.POST['username']
-    new_user.first_name = request.POST['first_name']
-    new_user.last_name = request.POST['last_name']
-    new_user.set_password(request.POST['password'])
+    def create_user(self, request):
+        """PUT method for create an user by receiving
+        four arguments from application, using POST method for security"""
 
-    # Must check if the assertives is  write correcly and define a message
-    # error for each one
+        new_user = BusinemeUser()
+        new_user.username = request.POST['username']
+        new_user.first_name = request.POST['first_name']
+        new_user.last_name = request.POST['last_name']
+        new_user.set_password(request.POST['password'])
 
-    username = new_user.username
-    first_name = new_user.first_name
-    last_name = new_user.last_name
+        # Must check if the assertives is  write correcly and define a message
+        # error for each one
 
-    new_user.assertEquals(username, None)
-    new_user.assertEquals(first_name, None)
-    new_user.assertEquals(last_name, None)
+        username = new_user.username
+        first_name = new_user.first_name
+        last_name = new_user.last_name
 
-    new_user.assertEquals(username, '')
-    new_user.assertEquals(first_name, '')
-    new_user.assertEquals(last_name, '')
+        new_user.assertIsNone(username)
+        new_user.assertIsNone(first_name)
+        new_user.assertIsNone(last_name)
 
-    new_user.save()
+        new_user.assertEquals(username, '')
+        new_user.assertEquals(first_name, '')
+        new_user.assertEquals(last_name, '')
 
+        new_user.save()
 
-def update_user_first_last_name(request):
-    """Define method for update first_name and last_name given a
-       specific BuslineUser using the POST method for security"""
+    def update_user_first_last_name(self, request):
+        """Define method for update first_name and last_name given a
+           specific BuslineUser using the POST method for security"""
 
-    update_user = request.user
-    update_user.first_name = request.POST['first_name']
-    update_user.last_name = request.POST['last_name']
+        update_user = request.user
+        update_user.first_name = request.POST['first_name']
+        update_user.last_name = request.POST['last_name']
 
-    first_name = update_user.first_name
-    last_name = update_user.last_name
+        first_name = update_user.first_name
+        last_name = update_user.last_name
 
-    update_user.assertEquals(update_user, None)
-    update_user.assertEquals(first_name, None)
-    update_user.assertEquals(last_name, None)
-    update_user.assertEquals(first_name, '')
-    update_user.assertEquals(last_name, '')
+        update_user.assertIsNone(update_user)
+        update_user.assertIsNone(first_name)
+        update_user.assertIsNone(last_name)
 
-    update_user.save()
+        update_user.assertEquals(first_name, '')
+        update_user.assertEquals(last_name, '')
+
+        update_user.save()
