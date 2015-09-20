0851061706cf94c37069f23ab407aeff83b0aa79
Lucas Severo <lucassalves65@gmail.com>
2015-09-20 19:08:58 -0300

    file that should not be deleted back to the repo
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

---
+++ busineme/authentication/views.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Busine-me API
+Universidade de Brasilia - FGA
+Técnicas de Programação, 2/2015
+@file views.py
+Views (on classic MVC, controllers) with methods that control the requisitions
+for the user authentication and manipulation.
+"""
+
+from django.views.generic import View
+from core.serializers import serialize_users
+from .models import BusinemeUser
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.contrib.auth import authenticate
+from core.return_message import return_message
+
+STATUS_OK = 200
+STATUS_NOT_FOUND = 404
+STATUS_CREATED = 201
+STATUS_SERVER_ERROR = 500
+
+
+class LoginView(View):
+    http_method_names = [u'get', u'post']
+
+    def get(self, request):
+        """Returns all users."""
+        json_data = serialize_users(BusinemeUser.objects.all())
+        return JsonResponse(json_data, content_type='application/json')
+
+    def post(self, request):
+        """Verify if user exists and authenticates."""
+
+        new_user = BusinemeUser()
+        new_user.create_user(request)
+        user = new_user.user_authenticate(request)
+
+        if user is not None:
+            message_log = return_message(STATUS_CREATED)
+            response = HttpResponse(
+                message_log, content_type='application/json',
+                status=STATUS_CREATED)
+        else:
+            message_log = return_message(STATUS_SERVER_ERROR)
+            response = HttpResponse(
+                message_log, content_type='application/json',
+                status=STATUS_SERVER_ERROR)
+
+        return response
+
+    def delete(self, request):
+        """Delete an user."""
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            user.delete()
+            message_log = return_message(STATUS_OK)
+            response = HttpResponse(
+                message_log, content_type='application/json',
+                status=STATUS_OK)
+        else:
+            message_log = return_message(STATUS_NOT_FOUND)
+            response = HttpResponse(
+                message_log, content_type='application/json',
+                status=STATUS_NOT_FOUND)
+
+        return response
