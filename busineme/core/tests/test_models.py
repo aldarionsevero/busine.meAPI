48cc2552a2f105c6bfa9382af21e742fb95c542c
Gustavo Sabino <gustavo.sabino@hotmail.com>
2015-11-26 22:13:44 -0200

    post str method test

--- busineme/core/tests/test_models.py
+++ busineme/core/tests/test_models.py
@@ -3,7 +3,9 @@ from ..models import Busline
 from ..models import Terminal
 from ..models import Favorite
 from ..models import Company
+from ..models import Post
 from authentication.models import BusinemeUser
+
 
 """
 This class is used for create tests some validations in Busline class.
@@ -80,3 +82,46 @@ class TestCompany(TestCase):
     def test_str(self):
         self.assertEquals(
             "CompanyTestName", self.company.__str__())
+
+
+class TestTerminal(TestCase):
+
+    def setUp(self):
+        self.terminal = Terminal()
+
+        self.terminal.description = "Terminal Description Test String"
+        self.terminal.addres = "Terminal Adress Test String "
+        self.terminal.save()
+
+    def test_str(self):
+        self.assertEquals(
+            "Terminal Description Test String", self.terminal.__str__())
+
+
+class TestPost(TestCase):
+
+    def setUp(self):
+        self.post = Post()
+
+        self.busline = Busline()
+        self.busline.line_number = "001"
+        self.busline.route_size = 0.1
+        self.busline.fee = 3.50
+        self.busline.save()
+
+        self.user = BusinemeUser()
+        self.user.username = "TestUser"
+        self.user.save()
+
+        self.post.busline = self.busline
+        self.post.traffic = 1
+        self.post.capacity = 1
+        self.post.user = self.user
+        self.post.save()
+        self.date_now = self.post.date
+        self.time_now = self.post.time
+
+    def test_str(self):
+        self.assertEquals(
+            "id: 1 date: %s %s busline_id: 1" % (self.date_now, self.time_now),
+            self.post.__str__())
