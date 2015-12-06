7fc25d54ce4a4778c743ec9c87068b4b42c1dc56
Gustavo Sabino <gustavo.sabino@hotmail.com>
2015-12-06 16:48:12 -0200

    adding test in return message when everything is okay

---
+++ busineme/core/tests/test_return_message.py
@@ -0,0 +1,27 @@
+"""
+Busine-me API
+Universidade de Brasilia - FGA
+Tecnicas de Programacao, 2/2015
+@file return_message.py
+Testing return messages in core.
+"""
+
+from django.test import TestCase
+from core.return_message import ReturnMessage
+
+STATUS_CODE_OK = 200
+STATUS_CODE_CREATED = 201
+STATUS_CODE_NOT_FOUND = 404
+STATUS_CODE_SERVER_ERROR = 500
+STATUS_UNDEFINED = 100000
+
+
+class TestReturnMessage(TestCase):
+
+    def setUp(self):
+        self.response_message = ReturnMessage()
+
+    def test_return_for_200(self):
+        message = self.response_message.return_message(
+            STATUS_CODE_OK)
+        self.assertEquals(message['return_message'], 'Everything Worked.')
