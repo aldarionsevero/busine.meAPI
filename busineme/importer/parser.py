41cb180be66036e1e054e03f55f7ee6fc3902e57
Filipe Ribeiro <firibeiro777@gmail.com>
2015-12-06 18:10:09 -0200

    last test for parser

--- busineme/importer/parser.py
+++ busineme/importer/parser.py
@@ -42,8 +42,6 @@ class Parser(object):
                 bus_line.save()
             except ObjectDoesNotExist:
                 print('Busline', row[0], 'has incomplete data.')
-            except IntegrityError:
-                print('Busline', row[0], 'already registered.')
 
     def create_busline_terminal_relation(self):
         """This method is used for read in parser file the fields referring a
